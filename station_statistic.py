4599167e7091176098c93039d208d1151a17925f
delphin2065 <delphin2065@hotmail.com>
2025-03-02 23:05:59 +0800

    renew file

--- station_statistic.py
+++ station_statistic.py
@@ -14,6 +14,7 @@ plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 df = data()
 st.header('台鐵車站人數統計')
 # 日期/ 站別碼/ 進站人數/ 出站人數
+st.write('資料來源: 臺鐵每日各站點進出站人數, from https://data.gov.tw/dataset/8792')
 
 station = st.radio('查詢條件', ['依照日期', '依照站別'])
 if station == '依照日期':
@@ -29,8 +30,8 @@ if station == '依照日期':
         fig = plt.figure(figsize=(10, 5))
         ax = fig.add_subplot(1, 1, 1)  
 
-        ax.plot(df_sta['日期'], df_sta['入站人數'], label='入站人數', marker='o')
-        ax.plot(df_sta['日期'], df_sta['出站人數'], label='出站人數', marker='*')
+        ax.plot(df_sta['日期'], df_sta['入站人數'], label='Number of people entring', marker='o')
+        ax.plot(df_sta['日期'], df_sta['出站人數'], label='Number of people leaving', marker='*')
         n = df_sta.shape[0]
         x = np.linspace(0, n-1, 5).astype(int)
         x_tick = [df_sta.loc[i, '日期'] for i in x]
@@ -38,10 +39,20 @@ if station == '依照日期':
 
         ax.grid(True)
         ax.legend()
-        ax.set_title(staCode + ' ' +'人數統計')
+        ax.set_title(staCode + ' ' +'People Counting')
         ax.set_xlabel('Date')
         ax.set_ylabel('Person Counts')
+
+        max_entry_index = df_sta['入站人數'].idxmax()
+        max_exit_index = df_sta['出站人數'].idxmax()
+
+        st.write(staCode + " 入站人數最多的日期: " + str(df_sta.loc[max_entry_index, "日期"]) + ", 共" + str(df_sta.loc[max_entry_index, "入站人數"]) + '人')
+
+        st.write(staCode + " 出站人數最多的日期: " + str(df_sta.loc[max_entry_index, "日期"]) + ", 共" + str(df_sta.loc[max_entry_index, "出站人數"]) + '人')
+
         st.pyplot(fig)            
+
+
 
 if station == '依照站別':
     
@@ -57,9 +68,19 @@ if station == '依照站別':
         dfg.insert(1, '結束日期', str(sd[1]))
         dfg_top10 = dfg.sort_values(by='入站人數', ascending=True).tail(10).copy()
         dfg_top10.reset_index(inplace=True, drop=True)
+        dfg_tail10 = dfg.sort_values(by='出站人數', ascending=True).tail(10).copy()
+        dfg_tail10.reset_index(inplace=True, drop=True)
+
+        max_entry_index = dfg_top10['入站人數'].idxmax()
+        max_exit_index = dfg_tail10['出站人數'].idxmax()
+        st.write('入站人數Top1為' + dfg_top10.loc[max_entry_index, '站別碼'] + ', 共' + str(dfg_top10.loc[max_entry_index, '入站人數']) + ' 人')
+        st.write('出站人數Top1為' + dfg_top10.loc[max_entry_index, '站別碼'] + ', 共' + str(dfg_top10.loc[max_entry_index, '出站人數']) + ' 人')
+
+
+        # 入站人數畫圖
         fig = plt.figure(figsize=(10, 5))
         ax = fig.add_subplot(1, 1, 1)
-        bars = ax.bar(dfg_top10['站別碼'], dfg_top10['入站人數'], label='入站人數')
+        bars = ax.bar(dfg_top10['站別碼'], dfg_top10['入站人數'], label='Number of people entering')
         
         for bar in bars:
             yval = bar.get_height()
@@ -69,16 +90,15 @@ if station == '依照站別':
         ax.grid(True)
         ax.set_xlabel('Station Code')
         ax.set_ylabel('Person Counts')
-        ax.set_title('入站人數前10名' + ' ' + '(' + str(sd[0]) + ' ~ ' + str(sd[1]) + ')')
+        ax.set_title('Top 10 stations with the most visitors' + ' ' + '(' + str(sd[0]) + ' ~ ' + str(sd[1]) + ')')
         ax.legend()
         st.pyplot(fig)
 
 
-        dfg_tail10 = dfg.sort_values(by='出站人數', ascending=True).tail(10).copy()
-        dfg_tail10.reset_index(inplace=True, drop=True)
+        # 出站人數畫圖
         fig = plt.figure(figsize=(10, 5))
         ax = fig.add_subplot(1, 1, 1)
-        bars = ax.bar(dfg_tail10['站別碼'], dfg_tail10['出站人數'], label='出站人數')
+        bars = ax.bar(dfg_tail10['站別碼'], dfg_tail10['出站人數'], label='Number of people leaving')
 
         for bar in bars:
             yval = bar.get_height()
@@ -87,8 +107,8 @@ if station == '依照站別':
         ax.grid(True)
         ax.set_xlabel('Station Code')
         ax.set_ylabel('Person Counts')
-        ax.set_title('出站人數前10名' + ' ' + '(' + str(sd[0]) + ' ~ ' + str(sd[1]) + ')')
-        ax.legend()
+        ax.set_title('Top 10 stations with the most departures' + ' ' + '(' + str(sd[0]) + ' ~ ' + str(sd[1]) + ')')
+        ax.legend()         
         st.pyplot(fig)
 
 
